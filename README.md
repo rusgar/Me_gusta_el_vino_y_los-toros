294d357950ab74f9ed19178942e8e9bd63689683
Edu Rus <edamamon@gmail.com>
2021-10-22 10:16:33 +0200

    Bases para Procfile

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ analisis de los dataset de vinos tintos y blancos
 https://www.kaggle.com/piyushagni5/white-wine-quality
 https://www.kaggle.com/uciml/red-wine-quality-cortez-et-al-2009
 
+
+# Required Files
+1.Setup.sh
+2 Procfile
+3 requirements.txt
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,12 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.7998807947541811
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=MultinomialNB(alpha=0.01, fit_prior=True)),
-    StackingEstimator(estimator=MultinomialNB(alpha=100.0, fit_prior=True)),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.8, min_samples_leaf=20, min_samples_split=3, n_estimators=100)
-)
+# Average CV score on the training set was: 0.7975004334736212
+exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.15000000000000002, min_samples_leaf=11, min_samples_split=8, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
