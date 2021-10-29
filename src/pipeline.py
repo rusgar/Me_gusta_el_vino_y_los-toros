f5bab4c69d5097d841ec7b56ec81285f7b434a82
Edu Rus <edamamon@gmail.com>
2021-10-29 15:47:48 +0200

    retocando

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +13,12 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.7975004334736212
-exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.15000000000000002, min_samples_leaf=11, min_samples_split=8, n_estimators=100)
+# Average CV score on the training set was: 0.6936827130719883
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=KNeighborsClassifier(n_neighbors=19, p=1, weights="uniform")),
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.5, max_depth=1, max_features=0.45, min_samples_leaf=10, min_samples_split=7, n_estimators=100, subsample=0.7500000000000001)),
+    MLPClassifier(alpha=0.01, learning_rate_init=0.1)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
